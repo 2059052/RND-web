5995a87487fa8090a3b995bca835320b143a0007
Nguyễn Phú Thành <2059052@itec.hcmus.edu.vn>
2025-01-11 16:48:40 +0700

    backend added

--- rnd/src/pages/Login.jsx
+++ rnd/src/pages/Login.jsx
@@ -1,13 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
 function Login() {
+  const [formData, setFormData] = useState({ username: "", password: "" });
+
+  const handleChange = (e) => {
+    console.log(e.target.name, e.target.value);
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post(
+        "http://127.0.0.1:8000/token",
+        formData,
+        {
+          headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+          },
+        }
+      );
+      localStorage.setItem("accessToken", response.data.access_token);
+      alert("Login successful!");
+    } catch (error) {
+      alert(error.response.data.detail);
+    }
+  };
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-orange-200">
       <h1 className="text-3xl font-bold mb-6">Login Page</h1>
-      <form className="bg-white p-8 rounded-lg shadow-md w-96">
+      <form
+        onSubmit={handleSubmit}
+        className="bg-white p-8 rounded-lg shadow-md w-96"
+      >
         <div className="mb-4">
           <label className="block text-gray-700">Username</label>
           <input
+            onChange={handleChange}
+            name="username"
             type="text"
             placeholder="Enter your username"
             className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring focus:ring-blue-300"
@@ -16,6 +48,8 @@ function Login() {
         <div className="mb-4">
           <label className="block text-gray-700">Password</label>
           <input
+            onChange={handleChange}
+            name="password"
             type="password"
             placeholder="Enter your password"
             className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring focus:ring-blue-300"
